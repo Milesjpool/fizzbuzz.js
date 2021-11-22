2b68c6d1ce61630084fd9ed67efac34d28d30a22
Miles Pool <milesjpool@gmail.com>
2021-11-22 14:12:21 +0000

    Add first feature.

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,13 @@
+function getFizzBuzz() {
+    return "fizz"
+}
+
 describe("fiz buzz", () => {
     it("runs a test", () =>{
         expect(1).toEqual(1);
+    })
+
+    it("returns fizz for 3", () => {
+        expect(getFizzBuzz(3)).toEqual("fizz")
     })
 })
