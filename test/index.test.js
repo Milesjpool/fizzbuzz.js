6520b63e32219f8db300e8e8e0ab47c6326dae6f
Miles Pool <milesjpool@gmail.com>
2021-11-22 14:14:25 +0000

    Implement non 3

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,17 @@
-function getFizzBuzz() {
-    return "fizz"
+function getFizzBuzz(value) {
+    return value == 3 ? "fizz" : undefined;
 }
 
 describe("fiz buzz", () => {
     it("runs a test", () =>{
         expect(1).toEqual(1);
-    })
+    });
 
     it("returns fizz for 3", () => {
-        expect(getFizzBuzz(3)).toEqual("fizz")
-    })
+        expect(getFizzBuzz(3)).toEqual("fizz");
+    });
+
+    it("doesn't returns fizz for non-multiples of 3", () => {
+        expect(getFizzBuzz(2)).not.toEqual("fizz");
+    });
 })
