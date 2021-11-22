0f10bd3a8435a637c571f4c4d00c457e9e5a6520
Miles Pool <milesjpool@gmail.com>
2021-11-22 14:20:16 +0000

    Implement buzz 5.

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,15 @@
 function getFizzBuzz(value) {
+    if (value === 5) return "buzz";
     return value == 3 ? "fizz" : undefined;
 }
 
 describe("fiz buzz", () => {
     it("returns fizz for 3", () => {
         expect(getFizzBuzz(3)).toEqual("fizz");
+    });
+
+    it("returns buzz for 5", () => {
+        expect(getFizzBuzz(5)).toEqual("buzz");
     });
 
     it("doesn't returns fizz for non-multiples of 3", () => {
