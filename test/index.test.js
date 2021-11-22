87b75f305c56ee62c300e4d5765d945eab400d7e
Miles Pool <milesjpool@gmail.com>
2021-11-22 14:32:35 +0000

    Implement 15

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 function getFizzBuzz(value) {
+    if (value === 15) return "fizzbuzz";
     if (value === 5) return "buzz";
     if (value === 3) return "fizz";
     return value;
@@ -11,6 +12,10 @@ describe("fiz buzz", () => {
 
     it("returns buzz for 5", () => {
         expect(getFizzBuzz(5)).toEqual("buzz");
+    });
+
+    it("returns fizzbuzz for 15", () => {
+        expect(getFizzBuzz(15)).toEqual("fizzbuzz");
     });
 
     it("returns the value for non-multiples of 3", () => {
