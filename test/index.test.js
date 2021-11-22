bf5c444cd4472d3a852dc9486169f99f7b611f1f
Miles Pool <milesjpool@gmail.com>
2021-11-22 19:40:02 +0000

    times up

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,28 @@
 function getFizzBuzz(value) {
     if (value % 3 === 0 && value % 5 === 0) return "fizzbuzz";
-    if (value === 5) return "buzz";
-    if (value === 3) return "fizz";
+    if (value === 5) return "buzz123";
+    if (value === 3) return "fizz123";
     return value;
 }
 
 describe("fiz buzz", () => {
-    it("returns fizz for 3", () => {
-        expect(getFizzBuzz(3)).toEqual("fizz");
+    it("returns fizz for multiples of 3 ", () => {
+        let n = Math.floor(Math.random() * 1000);
+        expect(getFizzBuzz(n * 3)).toContain("fizz");
     });
 
     it("returns buzz for 5", () => {
-        expect(getFizzBuzz(5)).toEqual("buzz");
-    });
-
-    it("returns fizzbuzz for 15", () => {
-        expect(getFizzBuzz(15)).toEqual("fizzbuzz");
+    let n = Math.floor(Math.random() * 1000);
+        expect(getFizzBuzz(n* 5)).toContain("buzz");
     });
 
     it("returns fizzbuzz for multiples of 5 and 3", () => {
-        let n = Math.floor(Math.random() * 1000)
+        let n = Math.floor(Math.random() * 1000);
         expect(getFizzBuzz(n * 3 * 5)).toEqual("fizzbuzz");
+    });
+
+    it("returns the value for non-multiples of 3", () => {
+        expect(getFizzBuzz(2)).toEqual(2);
     });
 
     it("returns the value for non-multiples of 3", () => {
