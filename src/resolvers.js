a0559da1acb43c39e935c8e0acbf2e676f03a664
Miles Pool <milesjpool@gmail.com>
2021-11-23 00:38:10 +0000

    Refactor partial resolvers.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,11 @@
 
 const { fizzy, buzzy } = require("./conditions.js");
 
-const createResolver = (condition, resolve, next) => (v) => condition(v) ? resolve : next(v);
+const partialResolver = (condition, resolve) => (next) => (v) => condition(v) ? resolve : next(v);
 
-const fizzBuzzyResolver = (next) => createResolver((v) => fizzy(v) && buzzy(v), "FizzBuzz", next);
-const fizzyResolver = (next) => createResolver(fizzy, "Fizz", next);
-const buzzyResolver = (next) => createResolver(buzzy, "Buzz", next);
+const fizzBuzzyResolver = partialResolver((v) => fizzy(v) && buzzy(v), "FizzBuzz");
+const fizzyResolver = partialResolver(fizzy, "Fizz");
+const buzzyResolver = partialResolver(buzzy, "Buzz");
 const baseResolver = () => (v) => `${v}`;
 const defaultFizzBuzzResolver = () => fizzBuzzyResolver(fizzyResolver(buzzyResolver(baseResolver())));
 
